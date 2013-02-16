a0cae43ce53d47aff3c8054e69ab9149a245a0f3
Andrey Balakirev <balakirev.andrey@gmail.com>
2013-02-17 01:27:15 +0400

    ofio.jquery render

--- ofio.jquery.js
+++ ofio.jquery.js
@@ -1,16 +1,19 @@
 define(['ofio/ofio', 'ofio/ofio.id', 'ofio/ofio.logger', 'vendor/jquery.min'], function (Ofio) {
   var module = new Ofio.Module({
-    name         : 'ofio.jquery',
-    dependencies : arguments
+    name: 'ofio.jquery',
+    dependencies: arguments
   });
 
   var event_splitter = /^(\S+)\s*(.*)$/;
 
 
-  module.init = function(){
-    this.$el = this.options.$el || $(this.options.el);
-    this.el = this.$el[0];
+  module.init = function () {
+    if (!this.options.$el && !this.options.el)
+      this.$el = $(this.render());
+    else
+      this.$el = this.options.$el || $(this.options.el);
 
+    this.el = this.$el[0];
     if (!this.el) this.log('`el` is undefined', 'warn', module);
 
     this.delegate_events();
@@ -22,7 +25,12 @@ define(['ofio/ofio', 'ofio/ofio.id', 'ofio/ofio.logger', 'vendor/jquery.min'], f
   };
 
 
-  module.delegate_events = function(events) {
+  module.render = function () {
+    return '';
+  };
+
+
+  module.delegate_events = function (events) {
     events = events || (typeof this.events == 'function' ? this.events() : this.events);
     if (!events) return;
 
@@ -54,7 +62,7 @@ define(['ofio/ofio', 'ofio/ofio.id', 'ofio/ofio.logger', 'vendor/jquery.min'], f
   };
 
 
-  module.undelegate_events = function() {
+  module.undelegate_events = function () {
     this.$el.off('.delegate_events' + this.id);
   };
 
--- ofio.logger.js
+++ ofio.logger.js
@@ -1,26 +1,27 @@
 define(['ofio/ofio'], function (Ofio) {
   if (!window.console) {
-    var stub = function(){
+    var stub = function () {
+      return arguments;
     };
 
     window.console = {
-      error    : stub,
-      log      : stub,
-      info     : stub,
-      warn     : stub,
-      trace    : stub,
-      group    : stub,
-      groupEnd : stub
+      error: stub,
+      log: stub,
+      info: stub,
+      warn: stub,
+      trace: stub,
+      group: stub,
+      groupEnd: stub
     };
   }
 
   var module = new Ofio.Module({
-    name         : 'ofio.logger'
+    name: 'ofio.logger'
   });
 
-  module.log = function(text, level, module){
+  module.log = function (text, level, module) {
     if (module) {
-        text = '[Module: ' + module.config.name + '] ' + text;
+      text = '[Module: ' + module.config.name + '] ' + text;
     }
     console[level || 'log'].call(console, text);
   };
